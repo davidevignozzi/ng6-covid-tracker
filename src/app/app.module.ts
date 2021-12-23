c68129ddf8600caba9fd5c3405c07e9aa38cdc78
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-12-23 11:58:35 +0100

    Setup + Bootstrap + Navbar * Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CountriesComponent } from './countries/countries.component';
 
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'countries', component: CountriesComponent}
+]
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavBarComponent,
+      HomeComponent,
+      CountriesComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
