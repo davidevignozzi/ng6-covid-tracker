2867c30727112078dd5b25949974065e8b076882
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-12-23 14:51:56 +0100

    total cases

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../data-service.service';
+import { GlobalDataSummary } from '../global-data';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,26 @@ import { DataServiceService } from '../data-service.service';
 export class HomeComponent implements OnInit {
 
   totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  globalData : GlobalDataSummary[];
+
   constructor(private dataService: DataServiceService) { }
 
   ngOnInit() {
     this.dataService.getGlobalData().subscribe({
       next: (result) =>{
         console.log(result);
+        this.globalData = result;
+        result.forEach(cs => {
+          if(!Number.isNaN(cs.confirmed)){
+            this.totalActive += cs.active;
+            this.totalConfirmed += cs.confirmed;
+            this.totalDeaths += cs.deaths;
+            this.totalRecovered += cs.recovered;
+          }
+        })
       }
     })
   }
